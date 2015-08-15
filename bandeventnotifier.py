84e1115e5918d4b5b80768597382a7f08589996a
Ville Valkonen <weezelding@gmail.com>
2015-08-15 16:48:43 +0300

    This commit tries to fix many things at once, although that's wrong, since the data storage paradigm must be changed.
    SQLite and Python threads doesn't go well together, at least without
    implementing your own locking. Basically three feasible options exists
    1) Use SQLAlchemy.
    2) Save fetched data to files and from there, insert data to database in
    a single thread.
    3) Don't run fetching operations in parallel.
    
    The first option would be the best, though would need major changes and
    will add an extra dependency to SQLAlchemy. Will skip this for now.
    
    The third option would be really slow, since network operations are the
    slowest. With many venues, this will come a pain in the neck. Verdict:
    won't go on this path.
    
    The second option is the most feasible for now. Once everything is
    working, will revisit this problem again and think about an SQLAlchemy
    option.
    --
    
    - Other changes
    Fix references used in bandeventnotifier.py file.
    
    Importing dbengine and lastfmfetch modules were broken.
    
    Fix database references. Now only one instance is initialized.
    
    Retrying data with requests loop was also broken. The crucial check of
    HTTP status code was out of the loop when entered in to a loop. This has
    been fixed too.
    
    Also implement a default behaviour. Now user can choose between a
    "fetch" and a "gigs" operation. With "fetch", bandeventnotifier.py fetches
    the listened artists from LastFM and parses the available venues.
    Eventually, these data are inserted in to a database. LastFM and venues
    data are intersected and interesting events are shown to user.

--- bandeventnotifier.py
+++ bandeventnotifier.py
@@ -2,60 +2,95 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import sys
 import threading
 from Queue import Queue
 
+import dbengine
 from plugin_handler import load_venue_plugins
-from dbengine import dbengine
-from lastfmfetch import lastfmfetch
+import lastfmfetch
 
 MAX_THREADS = 6
 
 
-# XXX Rough implementation of parallel venue fetcher
 class Fetcher(threading.Thread):
-    def __init__(self, q, db=None):
+    def __init__(self, q, dbobj=None):
         threading.Thread.__init__(self)
         self.fetchqueue = q
-        self.db = self.__dbInit(db)
+        self.dbeng = None
 
-    def __dbInit(self, db):
-        if self.db == None:
-            self.db = db
+        self.__dbInit(dbobj)
+
+    def __dbInit(self, dbobj):
+        if self.dbeng == None:
+            self.dbeng = dbobj
 
     def run(self):
         while True:
             venue = self.fetchqueue.get()
-            venue_data = start_fetching(venue)
-            print "[-] Fetching and parsing '%s'" % (venue.name)
+            self.fetchAndParse(venue)
+            print "[-] Fetching parsing '%s'" % (venue.name)
             self.fetchqueue.task_done()
             # TODO Implement callback to inform completed venues
             #print "[+] Fetching '%s' completed" % (venue.name)
 
-def start_fetching(venue):
-    retries = 3
-    r = requests.get(venue.url)
+    def fetchAndParse(self, venue):
+        retries = 3
+        r = requests.get(venue.url)
 
-    if r.status_code is not 200:
-        for retry in range(0, retries):
-            print "Couldn't connect %s, retrying %d/%d..." % (venue.url, retry + 1, retries)
-            r = requests.get(venue.url)
-    try:
-        venue.parseArtists(r.text)
-    except Exception, e:
-        raise e
+        if r.status_code is not 200:
+            for retry in range(0, retries):
+                print "Couldn't connect %s, retrying %d/%d..." % \
+                        (venue.url, retry + 1, retries)
+                r = requests.get(venue.url)
+
+                if r.status_code is 200:
+                    break
+
+        return self.__insert2db(r.text, venue) # State machine
+
+    def __insert2db(self, data, venue):
+        # Create needed venue entries to database.
+        self.dbeng.pluginCreateVenueEntity(venue.eventSQLentity())
+        venueparsed = venue.parseEvents(data)
+        self.dbeng.insertVenueEvents(venueparsed)
+
+def usage():
+    print "usage: bandeventnotifier.py [fetch|gigs]"
+    sys.exit(1)
 
 def main():
-    fetchqueue = Queue()
-    venues = load_venue_plugins()
+    dbeng = dbengine.DBEngine()
 
-    for v in range(MAX_THREADS):
-        t = Fetcher(fetchqueue, dbengine.DBEngine())
-        t.setDaemon(True)
-        t.start()
-    for venue in venues:
-        fetchqueue.put(venue)
-    fetchqueue.join()
+    if len(sys.argv) < 2:
+        dbeng.close()
+        usage()
+    elif sys.argv[1] == "fetch":
+        print "[-] Fetching LastFM user data."
+
+        lfmr = lastfmfetch.LastFmRetriever(dbeng)
+        [dbeng.insertLastFMartists(artist) \
+                for artist in lfmr.getAllListenedBands()]
+        print "[+] LastFM data fetched."
+
+        print "[-] Fetching venues data."
+        fetchqueue = Queue()
+        venues = load_venue_plugins()
+        for v in range(MAX_THREADS):
+            t = Fetcher(fetchqueue, dbeng)
+            t.setDaemon(True)
+            t.start()
+        for venue in venues:
+            fetchqueue.put(venue)
+        fetchqueue.join()
+        print "[+] Venues data fetched."
+    elif sys.argv[2] == "gigs":
+        print "Gigs you might be interested:"
+        dbeng.showgigs()
+    else:
+        usage()
+
+    dbeng.close()
 
 if __name__ == '__main__':
     main()
