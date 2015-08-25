474f52569b99ea7e0a4d6871e0912315c22d393b
Ville Valkonen <weezelding@gmail.com>
2015-08-25 23:21:12 +0300

    Implement and enable plugin_pakkahuone.py.

--- plugin_handler.py
+++ plugin_handler.py
@@ -15,8 +15,7 @@ def load_venue_plugins():
     Read plugin directory and load found plugins.
     Variable "blacklisted" can be used to exclude loading certain plugins.
     """
-    blacklisted = ["plugin_klubi", "plugin_pakkahuone", "plugin_tiketti", \
-                   "plugin_vastavirta"]
+    blacklisted = ["plugin_klubi", "plugin_tiketti", "plugin_vastavirta"]
     foundblacklisted = list()
     loadedplugins = list()
     pluginspathabs = os.path.join(os.path.dirname(__file__), "venues")
