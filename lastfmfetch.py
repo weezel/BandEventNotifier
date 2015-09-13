9fbbdfc22c0d62bb69694bdbb8b40170e94f9055
Ville Valkonen <weezelding@gmail.com>
2015-09-13 15:39:36 +0300

    Implement raw HTML parser.
    New LastFM is in a beta stage and API functionality seems to be broken.
    Don't rely on API functionality and therefore implement a HTML site
    parser so we can move along.

--- lastfmfetch.py
+++ lastfmfetch.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import lxml.html
 import pylast
+import re
+import requests
 
 FNAME = "APIKEYS"
 
 
 class LfmUserError(Exception): pass
 class LfmAPIkeyError(Exception): pass
+class PlaycountParseError(Exception): pass
 
 class LastFmRetriever(object):
     def __init__(self, db=None):
@@ -69,10 +73,37 @@ class LastFmRetriever(object):
         totalplaycount = sum([artist.playcount for artist in allbands])
         return float(thisband.playcount) / totalplaycount * 1000
 
+    def nonAPIparser(self, username):
+        """
+        New LastFM is broken in many ways. This method omits their API and
+        parses data directly from the website.
+        """
+        pageidx = 1
+        libraryurl = "http://www.last.fm/user/%s/library/artists?page=%s" % \
+                     (username, pageidx)
+        p = re.compile("[0-9,]+")
+        html = requests.get(libraryurl)
+        site = lxml.html.fromstring(html.text).getroottree().getroot()
+
+        for libitem in site.xpath('//tbody/tr'):
+            artist = libitem.xpath('./td[@class="chartlist-name"]/span/a/text()')
+            artist = " ".join(artist)
+
+            playcount = libitem.xpath('./td[@class="chartlist-countbar"]/span/span/a/text()')
+            playcount = " ".join(playcount)
+            pcount = re.search(p, playcount)
+            if pcount == None:
+                raise PlaycountParseError
+            pcount = pcount.group().replace(",", "")
+
+            print {u"name" : artist, \
+                   u"playcount" : pcount}
+
 if __name__ == '__main__':
     allbands = list()
     lfmr = LastFmRetriever()
 
-    for i in lfmr.getAllListenedBands(2):
-        print "%s"  % (i)
+    #for i in lfmr.getAllListenedBands(2):
+    #    print "%s"  % (i)
+    lfmr.nonAPIparser("weezel_ding")
 
