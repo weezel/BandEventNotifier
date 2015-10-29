0376c34696cfceb230ce1e79a5688e76b90ae888
Ville Valkonen <weezelding@gmail.com>
2015-10-29 18:02:47 +0200

    Fix, simplify and make Dog's home plugin more robust. Now it's more consistent with the other plugins.

--- venues/plugin_dogshome.py
+++ venues/plugin_dogshome.py
@@ -17,7 +17,7 @@ class Dogshome(object):
 
         # Parsing patterns
         self.datestartpat = re.compile("^\d+\.\d+. ")
-        self.monetarypattern = re.compile("[0-9.,]+ e")
+        self.monetarypattern = re.compile(u"[0-9.,]+€")
 
     def getVenueName(self):
         return self.name
@@ -37,10 +37,9 @@ class Dogshome(object):
                  u"country" : self.country }
 
     def parsePrice(self, line):
-        linetmp = line.replace(u"€", u" e")
-        prices = re.findall(self.monetarypattern, linetmp)
+        prices = re.findall(self.monetarypattern, line)
 
-        return map(lambda p: p.replace(" ", "").strip(","), prices)
+        return "/".join(prices)
 
     def parseDate(self, line):
         month_now = time.strftime("%m")
@@ -53,7 +52,7 @@ class Dogshome(object):
             # Are we on the new year already?
             if int(month) < int(month_now):
                 year += 1
-            date = "%s-%s-%s" % (year, month, day)
+            date = "%s-%.2d-%.2d" % (int(year), int(month), int(day))
         else:
             date = ""
         return unicode(date)
@@ -66,57 +65,30 @@ class Dogshome(object):
         return unicode("")
 
     def parseEvents(self, data):
-        #doc = parse("venues/doggari.html").getroot()
+        # XXX Double line event entries will likely fail
         doc = html.fromstring(data)
-        eventtags = doc.cssselect('div.innertube p')
-        container = list()
+        eventpattern = re.compile("^[0-9]+\.[0-9].?")
 
-        # XXX First line advertises a pub quiz, not useful for us.
-        # XXX Last three lines aren't needed either, those are:
-        #       - Free entry until otherwise mentioned
-        #       - All rights reserved
-        #       - Past events
-        for line in eventtags[1 : len(eventtags) - 3]:
-            lineastext = line.text_content()
-
-            # We could omit the aforementioned adverts by re-defining iteration
-            # ranges but I don't trust that the empty lines count will remain
-            # constant.
-            # More robust this way.
-            if lineastext is None or len(lineastext) == 1:
-                continue
-
-            # Real event...
-            if re.search(self.datestartpat, lineastext) != None:
-                container.append(lineastext.strip("\n"))
-            # ...more info on the next line, description or start time, join to
-            # the previously parsed line.
-            else:
-                container[len(container) - 1] += " " + lineastext.strip("\n")
-
-           # FIXME for some reason this skips the joined lines.
-           # eventinfo = container[len(container) - 1]
-           # yield { u"venue" : self.getVenueName(),   \
-           #     u"date" : self.parseDate(eventinfo),      \
-           #     u"event" : self.parseEvent(eventinfo),    \
-           #     u"price" : self.parsePrice(eventinfo) }
-
-        # XXX As you can spot, key values equals to database column names,
-        # in some level. There are few exceptions, for example a key 'venue'
-        # is a foreign key and therefore not equal to venueid.
-        # Fetching the venueid is done by the dbengine.py.
-        for event in container:
-            yield { u"venue" : self.getVenueName(),    \
-                    u"date" : self.parseDate(event),   \
-                    u"name" : self.parseEvent(event),  \
-                    u"price" : "/".join(self.parsePrice(event)) }
+        for line in doc.xpath('//div[@class="innertube"]/p'):
+            #lineastext = line.text_content()
+            if line.attrib.has_key("style") and \
+               line.attrib['style'] == "text-align: center;":
+                   if re.search(eventpattern, line.text_content()):
+                       eventastext = line.text_content()
+                       yield { u"venue" : self.getVenueName(),         \
+                               u"date" : self.parseDate(eventastext),  \
+                               u"name" : self.parseEvent(eventastext), \
+                               u"price" : self.parsePrice(eventastext) }
 
 if __name__ == '__main__':
     import requests
 
     d = Dogshome()
-    r = requests.get(d.url)
-    daa = d.parseEvents(r.text)
+    #r = requests.get(d.url)
+    with open("venues/dogshome.html", "r") as f:
+        r = f.read()
+    #daa = d.parseEvents(r.text)
+    daa = d.parseEvents(r)
 
     for i in daa:
         print "Keys = %s" % i.keys()
