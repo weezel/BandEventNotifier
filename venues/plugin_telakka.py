c80375763e5cc4ed970e5ee542023b791d571869
Ville Valkonen <weezelding@gmail.com>
2019-12-23 23:21:32 +0200

    Fix plugin for Telakka. Though year detection is still naïve.

--- venues/plugin_telakka.py
+++ venues/plugin_telakka.py
@@ -5,6 +5,7 @@ import lxml.html
 
 import re
 import time
+import datetime
 
 
 class PluginParseError(Exception): pass
@@ -17,9 +18,8 @@ class Telakka(object):
         self.country = "Finland"
 
         # Parsing patterns
-        self.datepat = re.compile("(Ma|Ti|Ke|To|Pe|La|Su) [0-9]+.[0-9]+.")
-        self.datepat2 = re.compile("[0-9]+.[0-9]+.")
-        self.monetary = re.compile("[0-9/]+[ ]?€")
+        self.pat_date = re.compile("[0-9]+.[0-9]+.")
+        self.monetary = re.compile("[0-9,]+[ ]?€")
 
     def getVenueName(self):
         return self.name
@@ -38,55 +38,55 @@ class Telakka(object):
                  "city" : self.city, \
                  "country" : self.country }
 
-    def parsePrice(self, tag):
-        # FIXME Cannot parse why this is not working yet.
-        price = re.search(self.monetary, tag)
-        if price:
-            price = re.search("[0-9/]+", price.group)
-        return "0" if not price else "%s€" % price
+    def parsePrice(self, elem):
+        if elem == '':
+            return '0 €'
 
-    def parseDate(self, tag):
-        month_now = int(time.strftime("%m"))
-        year = int(time.strftime("%Y"))
+        prices = re.search(self.monetary, elem)
+        if prices is None:
+            return "0 €"
+        return "".join(prices.group())
 
-        date = re.search(self.datepat2, tag)
-        if date == None:
+    def parseDate(self, elem):
+        parsed_date = ""
+        date_now = datetime.datetime.now()
+
+        try:
+            parsed = " ".join(elem.xpath('./strong/text()'))
+            # TODO Use walrus operator
+            # FIXME year swap
+            if re.search(self.pat_date, parsed):
+                parsed_date = re.search(self.pat_date, parsed).group()
+                parsed_date2 = f'{parsed_date}{date_now.year}'
+                return datetime.datetime.strptime(parsed_date2, "%d.%m.%Y")
+        except Exception:
             return ""
-
-        day, month = date.group().rstrip(".").split(".")
-
-         # Are we on the new year already?
-        if int(month) < month_now:
-            year += 1
-
-        return "%.4d-%.2d-%.2d" % (int(year), int(month), int(day))
-
-    def parseEvent(self, tag):
-        date = ""
-        price = ""
-        title = ""
-
-        date = self.parseDate(tag)
-        title = tag.split(".", 2)
-        if len(title) >= 3:
-            title = title[2]
-            title = re.sub("\s+", " ", title).lstrip(" ").rstrip(" ")
-        price = self.parsePrice(title)
-
-        return { "venue" : self.getVenueName(), \
-                 "date" : date,                 \
-                 "name" : "%s" % (title),       \
-                 "price" : price }
 
     def parseEvents(self, data):
         doc = lxml.html.fromstring(data)
 
-        for et in doc.xpath('//td[@class="main"]/table/tr'):
-            isEvent = " ".join(et.xpath('./td[starts-with(@class, ' + \
-                      '"item")]/text()'))
+        for et in doc.xpath('//div[@class="entry-content"]/p/span[@style="color: #ff6600;"]'):
+            event_date = self.parseDate(et)
+            if event_date is None:
+                continue
 
-            if re.search(self.datepat, isEvent):
-                yield self.parseEvent(isEvent)
+            tmp = "".join(et.xpath("../text()")).replace("\r\n", " ")
+            event = re.sub("\s+", " ", tmp).strip()
+            price = self.parsePrice(event)
+
+            tmp = "".join(et.xpath('../strong/text()')).replace("\r\n", " ")
+            has_title = re.sub("\s+", " ", tmp).strip()
+
+            if has_title != "":
+                ename = "{}: {}".format(has_title, event)
+            else:
+                ename = event
+
+            yield { "venue" : self.getVenueName(),
+                    "date" : event_date,
+                    "name" : ename,
+                    "price" : price }
+
 
 if __name__ == '__main__':
     import requests
