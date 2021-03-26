weezel/BandEventNotifier

commit 58b58c0a9b77d45e520e9c4c42dbc56e37cb8411

Remove pickle hack and use thread locks.

bandeventnotifier.py
@@ -4,7 +4,6 @@
 import datetime
 import glob
 import os
-import pickle
 import requests
 import signal
 import sys
@@ -19,6 +18,7 @@ import utils
 
 MAX_THREADS = 20
 MIN_PLAYCOUNT = 9
+
 
 def signal_handler(signal, frame):
     print("Aborting...")
@@ -48,6 +48,10 @@ class Fetcher(threading.Thread):
             venueparsed = list()
             try:
                 for i in venue.parseEvents(venuehtml):
+                    if i is None or len(i) == 0:
+                        print(f"Couldn't parse venue: {venue.getVenueName()}")
+                        self.fetchqueue.task_done()
+                        return
                     venueparsed.append(i)
             except TypeError as te:
                 print("{} Error while parsing {} venue".format( \
@@ -56,11 +60,7 @@ class Fetcher(threading.Thread):
                 self.fetchqueue.task_done()
                 return
 
-            # XXX Pickle hack to dodge SQLite concurrency problems.
             venue.parseddata = venueparsed
-            fname = os.path.join("venues", venue.__class__.__name__ + ".pickle")
-            pickle.dump(venue, open(fname, "wb"))
-
             self.fetchqueue.task_done()
 
     def __fetch(self, venue):
@@ -87,22 +87,6 @@ class Fetcher(threading.Thread):
                 if r.status_code is 200:
                     break
         return r.content
-
-def insert2db(dbeng):
-    # Create needed venue entries to database.
-    # XXX Remove pickle handler once the SQL concurrency problems are fixed.
-
-    # And here we load the venues again, yes annoying. More memory/CPU
-    # efficient implementation on it's way.
-    for fname in glob.iglob("venues/*.pickle"):
-        venue = pickle.load(open(fname, "rb"))
-        # Create needed entries in venue table
-        dbeng.pluginCreateVenueEntity(venue.eventSQLentity())
-
-        for v in venue.parseddata:
-            dbeng.insertVenueEvents(v)
-        # Remove pickle file
-        os.remove(fname)
 
 def usage():
     print("usage: bandeventnotifier.py [fetch [lastfm|venues] | gigs | html filename | purge]")
@@ -153,7 +137,10 @@ def main():
             print("[=] Venues data fetched.")
 
             print("[+] Inserting into a database...")
-            insert2db(dbeng)
+            dbeng.pluginCreateVenueEntity(venue.eventSQLentity())
+
+            for v in venue.parseddata:
+                dbeng.insertVenueEvents(v)
             print("[=] Venues added into the database.")
         else:
             usage()

dbengine.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import threading
 
 dbname = "bandevents.db"
 
@@ -23,6 +24,7 @@ class DBEngine(object):
         self.conn = None
         self.cur = None
         self.__firstRun()
+        self.lock = threading.Lock()
 
     def __firstRun(self):
         if self.conn == None:
@@ -31,10 +33,11 @@ class DBEngine(object):
             self.cur = self.conn.cursor()
 
     def close(self):
-        if self.cur:
-            self.cur.close()
-        if self.conn:
-            self.conn.close()
+        with self.lock:
+            if self.cur:
+                self.cur.close()
+            if self.conn:
+                self.conn.close()
 
     def pluginCreateVenueEntity(self, venuedict):
         """
@@ -46,28 +49,31 @@ class DBEngine(object):
         placeholders = ":" + ", :".join(venuedict.keys())
         q = "INSERT OR IGNORE INTO venue (%s) VALUES (%s);" \
                 % (cols, placeholders)
-        self.cur.execute(q, venuedict)
-        self.conn.commit()
+        with self.lock:
+            self.cur.execute(q, venuedict)
+            self.conn.commit()
 
     def insertVenueEvents(self, venue):
         """
         Insert parsed events from a venue into the database.
         """
-        # Replace venue by venueid
-        venue["venueid"] = self.getVenueByName(venue["venue"])[0]
-        # TODO Why do we have this keyword in the dict in general...
-        venue.pop("venue") # No such column in SQL db
-        cols = ", ".join(venue.keys())
-        placeholders = ":" + ", :".join(venue.keys())
-        q = "INSERT OR IGNORE INTO event (%s) VALUES (%s);" \
-                % (cols, placeholders)
-        self.cur.execute(q, venue)
-        self.conn.commit()
+        with self.lock:
+            # Replace venue by venueid
+            venue["venueid"] = self.getVenueByName(venue["venue"])[0]
+            # TODO Why do we have this keyword in the dict in general...
+            venue.pop("venue") # No such column in SQL db
+            cols = ", ".join(venue.keys())
+            placeholders = ":" + ", :".join(venue.keys())
+            q = "INSERT OR IGNORE INTO event (%s) VALUES (%s);" \
+                    % (cols, placeholders)
+            self.cur.execute(q, venue)
+            self.conn.commit()
 
     def insertLastFMartists(self, artist, playcount):
         q = "INSERT OR REPLACE INTO artist (name, playcount) VALUES (?, ?);"
-        self.cur.execute(q, [artist, playcount])
-        self.conn.commit()
+        with self.lock:
+            self.cur.execute(q, [artist, playcount])
+            self.conn.commit()
 
     def getVenues(self):
         q = "SELECT id, name, city, country FROM venue"
@@ -105,8 +111,9 @@ class DBEngine(object):
     def purgeOldEvents(self):
         q = "DELETE FROM event " \
             + "WHERE strftime('%Y-%m-%d', date) < date('now');"
-        self.cur.execute(q)
-        self.conn.commit()
+        with self.lock:
+            self.cur.execute(q)
+            self.conn.commit()
 
 if __name__ == '__main__':
     db = DBEngine()

commit 6880745738b9d53bfac77448402631391ae095c7

Fix issues introduced in commit 58b58c0. Now the threading is in place and locks correctly held. I previously missed a few inserts that were present when pickle was used. Obviously if nothing is put into database, nothing appears there. Weird.
This diff touches too many things but I was lazy. Forgive me.

bandeventnotifier.py
@@ -2,27 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import datetime
-import glob
 import os
-import requests
 import signal
 import sys
 import threading
 import time
 from queue import Queue
 
+import requests
+
 import dbengine
-from plugin_handler import load_venue_plugins
-from lastfmfetch import LastFmRetriever
 import utils
+from lastfmfetch import LastFmRetriever
+from plugin_handler import load_venue_plugins
+from venues.abstract_venue import AbstractVenue
 
 MAX_THREADS = 20
 MIN_PLAYCOUNT = 9
 
 
-def signal_handler(signal, frame):
+def signal_handler(signal_num: int, frame):
     print("Aborting...")
     sys.exit(1)
+
+
+venues_lock = threading.Lock()
+parsed_venues_data = dict()
 
 
 class Fetcher(threading.Thread):
@@ -33,51 +38,55 @@ class Fetcher(threading.Thread):
 
         self.__db_init(dbeng)
 
-    def __db_init(self, dbobj) -> None:
+    def __db_init(self, dbobj: dbengine.DBEngine) -> None:
         if self.dbeng is None:
             self.dbeng = dbobj
 
     def run(self) -> None:
+        global parsed_venues_data, venues_lock
+
         while True:
             venue = self.fetchqueue.get()
-            print(f"[+] Fetching and parsing venue '{venue.name}'")
-            venuehtml = self.__fetch(venue)
+            print(f"[+] Fetching and parsing venue '{venue.get_venue_name()}'")
+            venuehtml = self._fetch(venue)
 
             if venuehtml == "":
                 self.fetchqueue.task_done()
 
-            venueparsed = list()
             try:
-                for i in venue.parseEvents(venuehtml):
-                    if i is None or len(i) == 0:
-                        print(f"Couldn't parse venue: {venue.getVenueName()}")
+                for parsed_venue in venue.parse_events(venuehtml):
+                    if parsed_venue is None or len(parsed_venue) == 0:
+                        print(f"Couldn't parse venue: {venue.get_venue_name()}")
                         self.fetchqueue.task_done()
                         return
-                    venueparsed.append(i)
+                    with venues_lock:
+                        if venue.url not in parsed_venues_data:
+                            parsed_venues_data[venue.url] = []
+                        parsed_venues_data[venue.url].append(parsed_venue)
             except TypeError as te:
-                print("{} Error while parsing {}venue".format(
-                    utils.colorize("/_!_\\", "red"),
-                    venue.getVenueName()))
+                print("{warn} Error while parsing {venue_name} venue: {err}".format(
+                    warn=utils.colorize("/_!_\\", "red"),
+                    venue_name=venue.get_venue_name(),
+                    err=te))
                 self.fetchqueue.task_done()
                 return
 
-            venue.parseddata = venueparsed
             self.fetchqueue.task_done()
 
     # FIXME Add type
-    def __fetch(self, venue) -> str:
+    def _fetch(self, venue: AbstractVenue) -> bytes:
         retries = 3
         sleeptimesec = 5.0
         try:
             r = requests.get(venue.url)
         except Exception as general_err:
             print(f"ERROR: {general_err}")
-            return ""
+            return bytes("")
 
         # TODO Check r.ok
         if r.status_code == 404:
             print(f"{venue.url} is broken, please fix it.")
-            return ""
+            return bytes("")
         elif r.status_code != 200:
             for retry in range(0, retries):
                 print(f"Couldn't connect {venue.name}[{venue.city}] {venue.url}, ", end="")
@@ -127,7 +136,7 @@ def main() -> None:
                 dbeng.insertLastFMartists(artist, playcount)
             print("[=] LastFM data fetched.")
         elif sys.argv[2] == "venues":
-            print("[+] Fetching venues data.")
+            print("[+] Fetching venues events.")
             fetchqueue = Queue()
             venues = load_venue_plugins()
             for v in range(MAX_THREADS):
@@ -137,14 +146,19 @@ def main() -> None:
             for venue in venues:
                 fetchqueue.put(venue)
             fetchqueue.join()
-            print("[=] Venues data fetched.")
+            print("[=] Venues events fetched.")
 
             print("[+] Inserting into a database...")
-            dbeng.pluginCreateVenueEntity(venue.eventSQLentity())
+            for v in venues:
+                # Add database entries for the venue
+                dbeng.pluginCreateVenueEntity(v.event_sqlentity())
 
-            for v in venue.parseddata:
-                dbeng.insertVenueEvents(v)
-            print("[=] Venues added into the database.")
+                if v.url not in parsed_venues_data:
+                    print(f"Cannot add data for {v.name}/{v.city} ({v.country}) because it's empty. Continuing...")
+                    continue
+                parsed_venue = parsed_venues_data[v.url]
+                dbeng.insertVenueEvents(v, parsed_venue)
+            print("[=] Venues events added into the database.")
         else:
             usage()
     elif sys.argv[1] == "gigs":

dbengine.py
@@ -3,7 +3,9 @@
 
 import sqlite3
 import threading
-from typing import Dict, Generator
+from typing import Any, Dict, Generator, Type
+
+from venues.abstract_venue import AbstractVenue, IncorrectVenueImplementation
 
 dbname = "bandevents.db"
 
@@ -33,11 +35,9 @@ class DBEngine(object):
             self.conn = sqlite3.connect(dbname)
 
     def close(self) -> None:
-        while True:
-            with self.lock:
-                if self.conn:
-                    self.conn.close()
-                break
+        with self.lock:
+            if self.conn:
+                self.conn.close()
 
     def pluginCreateVenueEntity(self, venue: Dict[str, str]) -> None:
         """
@@ -49,59 +49,56 @@ class DBEngine(object):
         placeholders = ":" + ", :".join(venue.keys())
         q = "INSERT OR IGNORE INTO venue (%s) VALUES (%s);" \
             % (cols, placeholders)
-        while True:
-            with self.lock:
-                cur = None
-                try:
-                    cur = self.conn.cursor()
-                    cur.execute(q, venue)
-                    self.conn.commit()
-                except Exception as e:
-                    print(f"Failed with error message: {e}")
-                finally:
-                    cur.close()
-                    break
+        with self.lock:
+            cur = None
+            try:
+                cur = self.conn.cursor()
+                cur.execute(q, venue)
+                self.conn.commit()
+            except Exception as e:
+                print(f"Failed with error message: {e}")
+            finally:
+                cur.close()
 
-    # FIXME Create a proper venue interface
-    def insertVenueEvents(self, venue) -> None:
+    def insertVenueEvents(self, venue: AbstractVenue, events: Dict[str, Any]) -> None:
         """
         Insert parsed events from a venue into the database.
         """
-        while True:
-            with self.lock:
-                # Replace venue by venueid
-                venue["venueid"] = self.getVenueByName(venue["venue"])[0]
-                # TODO Why do we have this keyword in the dict in general...
-                venue.pop("venue")  # No such column in SQL db
-                cols = ", ".join(venue.keys())
-                placeholders = ":" + ", :".join(venue.keys())
-                q = "INSERT OR IGNORE INTO event (%s) VALUES (%s);" \
-                    % (cols, placeholders)
-                cur = None
-                try:
-                    cur = self.conn.cursor()
-                    cur.execute(q, venue)
-                    self.conn.commit()
-                except Exception as e:
-                    print(f"Failed with error message: {e}")
-                finally:
-                    cur.close()
-                    break
+        with self.lock:
+            cur = None
+            try:
+                cur = self.conn.cursor()
+
+                for event in events:
+                    venue_id = self.getVenueByName(
+                        event["venue"],
+                        venue.get_city(),
+                        venue.get_country())[0]
+                    event["venueid"] = venue_id
+                    event.pop("venue")  # venue -> venueid to match sql implementation
+                    cols = ", ".join(event.keys())
+                    placeholders = ":" + ", :".join(event.keys())
+                    q = f"INSERT OR IGNORE INTO event ({cols}) VALUES ({placeholders});"
+
+                    cur.execute(q, event)
+            except Exception as e:
+                print(f"Failed with error message: {e}")
+            finally:
+                self.conn.commit()
+                cur.close()
 
     def insertLastFMartists(self, artist: str, playcount: int) -> None:
         q = "INSERT OR REPLACE INTO artist (name, playcount) VALUES (?, ?);"
-        while True:
-            with self.lock:
-                cur = None
-                try:
-                    cur = self.conn.cursor()
-                    cur.execute(q, [artist, playcount])
-                    self.conn.commit()
-                except Exception as e:
-                    print(f"Failed with error message: {e}")
-                finally:
-                    cur.close()
-                    break
+        with self.lock:
+            cur = None
+            try:
+                cur = self.conn.cursor()
+                cur.execute(q, [artist, playcount])
+                self.conn.commit()
+            except Exception as e:
+                print(f"Failed with error message: {e}")
+            finally:
+                cur.close()
 
     def getVenues(self) -> Dict[str, str]:
         q = "SELECT id, name, city, country FROM venue"
@@ -118,14 +115,14 @@ class DBEngine(object):
 
         return results
 
-    def getVenueByName(self, vname: str) -> str:
+    def getVenueByName(self, vname: str, city: str, country: str) -> str:
         q = "SELECT id, name, city, country FROM venue " \
-            + "WHERE name = ? LIMIT 1;"
+            + "WHERE name = ? AND city = ? AND country = ? LIMIT 1;"
         venue_name = str()
         cur = None
         try:
             cur = self.conn.cursor()
-            results = cur.execute(q, [vname])
+            results = cur.execute(q, [vname, city, country])
             venue_name = results.fetchone()
         except Exception as e:
             print(f"Failed with error message: {e}")
@@ -183,15 +180,13 @@ class DBEngine(object):
     def purgeOldEvents(self) -> None:
         q = "DELETE FROM event " \
             + "WHERE strftime('%Y-%m-%d', date) < date('now');"
-        while True:
-            with self.lock:
-                cur = None
-                try:
-                    cur = self.conn.cursor()
-                    cur.execute(q)
-                    self.conn.commit()
-                except Exception as e:
-                    print(f"Failed with error message: {e}")
-                finally:
-                    cur.close()
-                    break
+        with self.lock:
+            cur = None
+            try:
+                cur = self.conn.cursor()
+                cur.execute(q)
+                self.conn.commit()
+            except Exception as e:
+                print(f"Failed with error message: {e}")
+            finally:
+                cur.close()

plugin_handler.py
@@ -8,9 +8,12 @@
 import os
 import pkgutil
 import sys
+from typing import List
+
+from venues.abstract_venue import AbstractVenue
 
 
-def load_venue_plugins():
+def load_venue_plugins() -> List[AbstractVenue]:
     """
     Read plugin directory and load found plugins.
     Variable "blocklist" can be used to exclude loading certain plugins.
@@ -31,7 +34,7 @@ def load_venue_plugins():
             found_blocked.append(plugname.lstrip("plugin_"))
             continue
 
-        plugpath = "venues.%s" % (plugname)
+        plugpath = f"venues.{plugname}"
         loadplug = __import__(plugpath, fromlist=[plugname])
 
         classname = plugname.split("_")[1].title()
@@ -39,10 +42,6 @@ def load_venue_plugins():
 
         instance = loadedclass()
         loadedplugins.append(instance)
-        print(f"Loaded plugin: {instance.getVenueName()}")
+        print(f"Loaded plugin: {instance.get_venue_name()}")
     print("Blocked plugins: {}.\n".format(", ".join(found_blocked[1:])))
     return loadedplugins
-
-
-if __name__ == '__main__':
-    load_venue_plugins()