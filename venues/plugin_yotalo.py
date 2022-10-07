f9bc46a43fddc8be54a976ca3d934a9371979fed
Ville Valkonen <weezelding@gmail.com>
2022-10-07 23:37:57 +0300

    Fix parsing of Yo-talo and enable it again.
    Signed-off-by: Ville Valkonen <weezelding@gmail.com>

--- venues/plugin_yotalo.py
+++ venues/plugin_yotalo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime
 import re
 from typing import Any, Dict, Generator
 
@@ -10,11 +11,11 @@ from venues.abstract_venue import AbstractVenue
 class Yotalo(AbstractVenue):
     def __init__(self):
         super().__init__()
-        self.url = "https://yo-talo.fi/ohjelma/"
+        self.url = "https://yo-talo.fi/tapahtumat"
         self.name = "Yo-talo"
         self.city = "Tampere"
         self.country = "Finland"
-        self.date_pat = re.compile("[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{4}")
+        self.date_pat = re.compile("[0-9]{1,2}\\.[0-9]{1,2}")
 
     def normalize_string(self, s: str):
         rm_newlines = s.replace("\n", " ")
@@ -24,15 +25,18 @@ class Yotalo(AbstractVenue):
         return rm_right_padding
 
     def parse_date(self, tag: lxml.html.HtmlElement) -> str:
-        date_tag = "".join(tag.xpath('./*/div[contains(@class, "event-content")]/p[2]/text()'))
+        date_tag = "".join(tag.xpath('.//div[contains(@class, "me-3")]/div[contains(@class, "fs-1")]/text()'))
         if date_found := re.search(self.date_pat, date_tag):
-            day, month, year = date_found.group().split(".")
+            day, month = date_found.group().split(".")
+            year = datetime.datetime.today().year
+
+            if int(month) < datetime.datetime.today().month:
+                year = str(int(year) + 1)
             return f"{int(year):04d}-{int(month):02d}-{int(day):02d}"
         return "00-00-0000"
 
     def parse_price(self, tag: lxml.html.HtmlElement) -> str:
-        price_elem = "".join(tag.xpath('./*/div[contains(@class, "event-content")]'
-                                       '/p[contains(text(), "Ennakkoliput")]/text()'))
+        price_elem = "".join(tag.xpath('./div/*/div[@class="d-block"]//div[contains(@class, "fs-6")]/text()'))
         prices_with_mon = self.pricepat_monetary.findall(price_elem)
         prices = []
         for price in prices_with_mon:
@@ -49,17 +53,23 @@ class Yotalo(AbstractVenue):
         return "{}€".format("".join(prices))
 
     def parse_artists(self, tag: lxml.html.HtmlElement) -> str:
-        title = "".join(tag.xpath('./*/div[@class="event-title"]/h3/text()'))
-        title = self.normalize_string(title)
-        info = "".join(tag.xpath('./*/div[@class="event-content"]/p[2]/strong/text()'))
-        info = self.normalize_string(info)
+        base = tag.xpath('./div/*/div[@class="d-block"]/div')
+
+        if len(base) != 2:
+            return ""
+
+        title_tmp = base[0].xpath('./div[contains(@class, "fw-bolder")]/text()')
+        title = "".join(title_tmp)
+        info_tmp = base[1].xpath('./text()')
+        info = "".join(info_tmp)
 
         return f"{title}: {info}"
 
     def parse_events(self, data: bytes) -> Generator[Dict[str, Any], None, None]:
         doc = lxml.html.fromstring(data)
 
-        for item in doc.xpath('//li[contains(@class, "event live")]'):
+        # for item in doc.xpath('//li[contains(@class, "event live")]'):
+        for item in doc.xpath('//div[@class="col-md-8"]'):
             date = self.parse_date(item)
             if date == "00-00-0000":
                 continue
