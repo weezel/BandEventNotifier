d1a784ec841f4f0fbe8945bf7a5f81e7c3952b93
Ville Valkonen <weezelding@gmail.com>
2022-10-07 22:32:56 +0300

    Disable more plugins to make it work again.
    Will fix venues parsing later.
    
    Signed-off-by: Ville Valkonen <weezelding@gmail.com>

--- plugin_handler.py
+++ plugin_handler.py
@@ -18,7 +18,7 @@ def load_venue_plugins() -> List[AbstractVenue]:
     Read plugin directory and load found plugins.
     Variable "blocklist" can be used to exclude loading certain plugins.
     """
-    blocklist = ["plugin_tiketti", "plugin_telakka"]
+    blocklist = ["plugin_tiketti", "plugin_telakka", "plugin_lutakko", "plugin_yotalo"]
     found_blocked = list()
     loadedplugins = list()
     pluginspathabs = os.path.join(os.path.dirname(__file__), "venues")
