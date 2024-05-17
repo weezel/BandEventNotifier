d2d3cc3317f8d72fad7c580e51377ac99b789b80
Ville Valkonen <weezel@users.noreply.github.com>
2024-05-17 23:16:20 +0300

    Update Vastavirta plugin.
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

--- venues/plugin_vastavirta.py
+++ venues/plugin_vastavirta.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import re
+from datetime import datetime
 from typing import Any, Dict, Generator
 
 import lxml.html
@@ -13,73 +15,55 @@ class Vastavirta(AbstractVenue):
         self.name = "Vastavirta"
         self.city = "Tampere"
         self.country = "Finland"
-        self.parseddata = None
-        self.monthmap = {
-            "Tammi": 1,
-            "Helmi": 2,
-            "Maalis": 3,
-            "Huhti": 4,
-            "Touko": 5,
-            "Kesä": 6,
-            "Heinä": 7,
-            "Elo": 8,
-            "Syys": 9,
-            "Loka": 10,
-            "Marras": 11,
-            "Joulu": 12
-        }
+        self.monetary_pat = "[0-9]+"
 
     def parse_price(self, tag: lxml.html.HtmlElement) -> str:
-        parsedprice = tag.xpath('.//div[@class="event-details"]/*/text()')
-        prices = " ".join(parsedprice)
+        tickets = tag.xpath('./p/text()')
+        for ticket in tickets:
+            if "Liput:" in ticket:
+                tickets = ticket
+                break
+            if "Ilmainen sisäänpääsy" in ticket:
+                return "0€"
 
-        return f"{prices}" if prices else "0"
+        if len(tickets) == "":
+            return "0€"
 
-    def parse_date(self, tag: lxml.html.HtmlElement) -> str:
-        day = tag.xpath('./div[contains(@class, "event-date")]/*/div[@class="event-day"]/text()')
-        m = tag.xpath('./div[contains(@class, "event-date")]/*/div[@class="event-month"]/text()')
-        year = tag.xpath('./div[contains(@class, "event-date")]/*/div[@class="event-year"]/text()')
+        parsed = re.findall(self.monetary_pat, tickets)
+        if len(parsed) == 2:
+            return f"{parsed[0]}€ / {parsed[1]}€"
 
-        day = " ".join(day)
-        month = " ".join(m).capitalize()
-        year = " ".join(year)
+        return f"{parsed[0]}€"
 
-        is_day_empty = day == ""
-        is_month_empty = month == ""
-        is_year_empty = year == ""
-        if any([is_day_empty, is_month_empty, is_year_empty]):
-            return ""
-
-        month = self.monthmap[month]
-        return "{:04d}-{:02d}-{:02d}".format(int(year), int(month), int(day))
+    def parse_date(self, d: str) -> str:
+        date = d.split(sep=" ")[0]
+        parsed = datetime.strptime(date, "%d.%m.%Y")
+        return "{:04d}-{:02d}-{:02d}".format(parsed.year, parsed.month, parsed.day)
 
     def parse_event(self, event: lxml.html.HtmlElement) -> Dict[str, Any]:
-        date = self.parse_date(event)
-        etitle = " ".join(event.xpath('./*/div[@class="event-title"]/*/text()'))
+        date, etitle = " ".join(event.xpath('./h3[@class="vv-events-heading"]/text()')).split(sep=" - ", maxsplit=1)
+        etitle = etitle.lstrip()
+        date = self.parse_date(date)
         prices = self.parse_price(event)
 
-        return {"venue": self.get_venue_name(),
-                "date": date,
-                "name": etitle,
-                "price": prices}
+        return {
+            "venue": self.get_venue_name(),
+            "date": date,
+            "name": etitle,
+            "price": prices,
+        }
 
-    def parse_events(self, data: bytes) \
-            -> Generator[Dict[str, Any], None, None]:
+    def parse_events(self, data: bytes) -> Generator[Dict[str, Any], None, None]:
         doc = lxml.html.fromstring(data).getroottree().getroot()
-        tags = doc.xpath('//div[@class="event-list"]/'
-                         + 'ul[@class="event-list-view"]/'
-                         + 'li[@class="event "]')
-        tmp = ""
+        events = doc.xpath('//div[@class="vv-custom-events"]')
 
-        for event in reversed(tags):
+        for event in events:
             parsed = self.parse_event(event)
 
             if parsed["date"] == "":
-                tmp += ", " + parsed["name"]
-            else:
-                parsed["name"] += tmp
-                yield parsed
-                tmp = ""
+                continue
+
+            yield parsed
 
 
 if __name__ == '__main__':
