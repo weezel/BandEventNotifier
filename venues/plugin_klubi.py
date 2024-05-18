2481cc8c9ce23ec884255f6e858d4d5c3f3d88fd
Ville Valkonen <weezel@users.noreply.github.com>
2024-05-18 23:19:45 +0300

    Update Klubi plugin.
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

--- venues/plugin_klubi.py
+++ venues/plugin_klubi.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import datetime
-import json
 import re
 from typing import Any, Dict, Generator
 
-from venues.abstract_venue import AbstractVenue, IncorrectVenueImplementation
+import lxml.html
+
+from venues.abstract_venue import AbstractVenue
 
 
 class Klubi(AbstractVenue):
@@ -16,48 +16,39 @@ class Klubi(AbstractVenue):
         self.name = "Klubi"
         self.city = "Tampere"
         self.country = "Finland"
+        self.price_pat = r"[0-9,.]€"
 
-    def parse_date_from_epoc(self, date: float):
-        return datetime.datetime.fromtimestamp(date).strftime("%Y-%m-%d")
+    def parse_artists(self, tag: lxml.html.HtmlElement) -> str:
+        title = " ".join(tag.xpath('.//h2[contains(@class, "event-artist")]/text()'))
+        return re.sub(r"\s+", " ", title)
 
-    def parse_artists(self, artists: str) -> str:
-        if artists is None:
-            return ""
-        return ", ".join([artist["nimi"] for artist in json.loads(artists)])
+    def get_price(self, tag: lxml.html.HtmlElement) -> str:
+        price_tags = " ".join(tag.xpath('./ul[@class="event-feed-item__info"]/li/text()'))
+        prices = re.findall(self.price_pat, price_tags)
+        if len(prices) == 0:
+            return "0€"
+        return "/".join(prices)
 
-    def parse_price(self, e: Dict[str, str]):
-        from_door = e["hinta"] if e["hinta"] is not None else 0
-        return "{}/{}€".format(e["ennakkolipun_hinta"], from_door)
+    def parse_events(self, data: bytes) -> Generator[Dict[str, Any], None, None]:
+        doc = lxml.html.fromstring(data)
+        events = doc.xpath('//div[contains(@class, "event-feed")]//div[@class="event-feed-item__content"]')
+        for event in events:
+            event_venue = " ".join(event.xpath('.//li[@class="event-venue"]/text()'))
+            if "klubi" not in event_venue.lower():
+                continue
 
-    def parse_event(self, e: Dict[str, str]) \
-            -> Dict[str, Any]:
-        date = self.parse_date_from_epoc(int(e["aika_alku"]))
-        title = e["otsikko"].rstrip(":")
-        artist = self.parse_artists(e["artistit"])
-        desc = json.loads(e["kuvaus"]).get("summary")
-        price = self.parse_price(e)
-
-        return {"venue": self.get_venue_name(),
-                "date": date,
-                "name": re.sub("\s+", " ", f"{title}: {artist} {desc}"),
-                "price": price}
-
-    def parse_events(self, data: bytes) \
-            -> Generator[Dict[str, Any], None, None]:
-        events_line_pattern = re.compile(r"^\s+\$scope.events = ")
-        events = None
-        for line in data.decode("utf-8").split("\n"):
-            if re.search(events_line_pattern, line):
-                events = line
-                break
-        if events is None:
-            raise IncorrectVenueImplementation("Parsing Klubi failed")
-        normalized = re.sub(events_line_pattern, "", events).rstrip(";\r")
-        doc = json.loads(normalized)
-
-        for e in doc:
-            if e["tila"] == "Klubi":
-                yield self.parse_event(e)
+            date = "".join(event.xpath('.//time/text()'))
+            if date.count("/") != 2:
+                continue
+            day, month, year = date.split("/")
+            title = self.parse_artists(event)
+            prices = self.get_price(event)
+            yield {
+                "venue": self.get_venue_name(),
+                "date": "{:04d}-{:02d}-{:02d}".format(int(year), int(month), int(day)),
+                "name": title,
+                "price": prices,
+            }
 
 
 if __name__ == '__main__':
