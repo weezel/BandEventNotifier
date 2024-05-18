6611e24783f4c8916fa3983f5622ae1f436b7514
Ville Valkonen <weezel@users.noreply.github.com>
2024-05-18 23:59:14 +0300

    Update Lutakko plugin.
    Signed-off-by: Ville Valkonen <weezel@users.noreply.github.com>

--- venues/plugin_lutakko.py
+++ venues/plugin_lutakko.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import lxml.html
-
 import re
 import time
+from datetime import datetime
+from typing import Any, Dict, Generator
+
+import lxml.html
 
 from venues.abstract_venue import AbstractVenue
 
@@ -20,68 +22,45 @@ class Lutakko(AbstractVenue):
         # Parsing patterns
         self.datepat = re.compile("[0-9.]+")
 
-    def parse_price(self, t):
-        tag = " ".join(t.xpath('./div[@role="tickets"]/div/a/strong/text()'))
+    def parse_price(self, tag: lxml.html.HtmlElement) -> str:
+        prices = tag.xpath('string(.//span[@class="price"]/span/bdi)')
+        return prices
 
-        return "0" if len(tag) == 0 else "%s" % tag
+    def parse_date(self, tag: lxml.html.HtmlElement) -> str:
+        date = " ".join(tag.xpath('.//span[@class="date"]/text()'))
 
-    def parse_date(self, t):
-        month_now = time.strftime("%m")
-        year = int(time.strftime("%Y"))
-        date = ""
+        day, month = date.rstrip(".").split(".")
 
-        tag = " ".join(t.xpath('./div[@class="badges"]' +
-                               '/div[@class="date"]' +
-                               '/span/text()'))
-        splt = tag.split(" - ")
-        slen = len(splt)
-        if slen == 1:
-            date = " ".join(re.findall(self.datepat, tag))
-        # We only care about the starting date
-        elif slen > 1:
-            date = " ".join(re.findall(self.datepat, tag)[0])
-        date = date.rstrip(".")  # FIXME
-
-        day, month = date.split(".")
-        day = day.replace(" ", "")
-        month = month.replace(" ", "")
-
+        year = datetime.now().year
         # Are we on the new year already?
+        month_now = time.strftime("%m")
         if int(month) < int(month_now):
             year += 1
-
         return "{:04d}-{:02d}-{:02d}".format(int(year), int(month), int(day))
 
-    def parse_event(self, tag):
-        date = self.parse_date(tag)
-        title = tag.xpath('./a')[0].text_content()
-        desc = tag.xpath('./p')[0].text_content()
-        price = self.parse_price(tag)
-
-        name = f"{title} {desc}"
-        name = re.sub("\\s+", " ", name).lstrip(" ").rstrip(" ")
-
-        return {
-            "venue": self.get_venue_name(),
-            "date": date,
-            "name": name,
-            "price": price
-        }
-
-    def parse_events(self, data: str):
+    def parse_events(self, data: str) -> Generator[Dict[str, Any], None, None]:
         doc = lxml.html.fromstring(data)
 
-        for event in doc.xpath('//ul[@role="upcoming-events"]/li'):
-            yield self.parse_event(event)
+        for event in doc.xpath('//a[contains(@class, "woocommerce-LoopProduct-link")]'):
+            date = self.parse_date(event)
+            title = event.xpath('string(.//h2[contains(@class, "woocommerce-loop-product__title")])').strip()
+            price = self.parse_price(event)
+
+            yield {
+                "venue": self.get_venue_name(),
+                "date": date,
+                "name": title,
+                "price": price
+            }
 
 
 if __name__ == '__main__':
     import requests
 
-    l = Lutakko()
-    r = requests.get(l.url)
+    lutakko = Lutakko()
+    r = requests.get(lutakko.url)
 
-    for e in l.parse_events(r.content):
+    for e in lutakko.parse_events(r.content):
         for k, v in e.items():
             print(f"{k:>10s}: {v}")
         print()
